cd464f7015f4f26f3a18dff6a5518f7710107b94
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2010-08-01 16:00:49 -0700

    Footer widgets in progress

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
         <div id="siteFooter" class="clearfix">
+        
+            <?php get_sidebar( 'footer' ); ?>
+            
             <div class="footerBox left">
                 <h4>Footer Title</h4>
                 <ul>
@@ -30,10 +33,11 @@
         
     </div>
     
-    <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
     
     <?php wp_footer(); ?>
     
+    <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+
     <!--
     22416e64206361666665696e652e2049206b6e6f77207468617420736f6d6520
     6f6620796f75207468696e6b2074686174206c69666520776974686f75742020
--- functions.php
+++ functions.php
@@ -1,6 +1,35 @@
 <?php
-if ( function_exists('register_sidebar') )
+if ( function_exists('register_sidebar') ) {
+    // Default sidebar widget area    
     register_sidebar();
+    
+    // Footer sidebar left
+    register_sidebar( array(
+        'name' => 'Footer Widget Left',
+        'id' => 'footerWidgetLeft',
+        'description' => 'Footer Widget Left',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ) );
+
+    // Footer sidebar center
+    register_sidebar( array(
+        'name' => 'Footer Widget Center',
+        'id' => 'footerWidgetCenter',
+        'description' => 'Footer Widget Center',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ) );
+
+    // Footer sidebar right
+    register_sidebar( array(
+        'name' => 'Footer Widget Right',
+        'id' => 'footerWidgetRight',
+        'description' => 'Footer Widget Right',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+    ) );
+}
     
 if (function_exists('register_nav_menus')) {
     register_nav_menus( array(
